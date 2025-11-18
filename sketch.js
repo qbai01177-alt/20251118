78c5d8c1993e0fd55e0d312d4b34f22db6d53490
qbai01177-alt <qbai01177@gmail.com>
2025-11-18 11:52:18 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,8 @@
+function preload() {
+  // 角色 1 (左邊角色)
+  spriteSheet1 = loadImage('1/all1.png');
+
+  // 角色 2 (右邊角色)
+  // ❗ 這裡的路徑必須完全匹配您的檔案結構 ❗
+  spriteSheet2 = loadImage('2/20251118/all2.png'); 
+}
